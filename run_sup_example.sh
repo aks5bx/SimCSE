af3b6cf1a916edc6693c333565d908a84aae29cc
Aditya Srikanth <aks9136@gv006.hpc.nyu.edu>
2022-11-22 18:12:03 -0500

    changing old version syntax

--- run_sup_example.sh
+++ run_sup_example.sh
@@ -20,13 +20,13 @@ python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT
     --train_file data/nli_for_simcse.csv \
     --output_dir result/my-sup-simcse-bert-base-uncased \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 128 \
+    --per_device_train_batch_size 512 \
     --learning_rate 5e-5 \
     --max_seq_length 32 \
     --evaluation_strategy steps \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
-    --eval_steps 125 \
+    --eval_steps 250 \
     --pooler_type cls \
     --overwrite_output_dir \
     --temp 0.05 \
