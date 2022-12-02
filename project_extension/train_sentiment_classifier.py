7987781385795295a99107c4162a024934fffe76
Andre Chen <alc9635@gr053.hpc.nyu.edu>
2022-12-02 17:32:35 -0500

    added logic to run faster on GPU

--- project_extension/train_sentiment_classifier.py
+++ project_extension/train_sentiment_classifier.py
@@ -13,12 +13,15 @@ from torch.utils.data import DataLoader
 from transformers import AutoModel, AutoTokenizer
 
 from neural_sentiment_classifier import NeuralSentimentClassifier
-from tweet_dataset import TweetDataset, tweet_batch_collate
+from tweet_dataset import TweetDataset, get_sent_embeddings
 
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print('Running on', device)
+BATCHSIZE = 1024 if device=='cuda' else 32
+NEPOCHS = 20 if device=='cuda' else 10
 
-def prepare_datasets(twitter_data):
+def prepare_datasets(twitter_data, tokenizer1, encoder1, tokenizer2=None, encoder2=None):
     twitter_data = twitter_data.dropna(subset='tweets')
     indices = np.arange(len(twitter_data))
     np.random.shuffle(indices)
@@ -30,10 +33,13 @@ def prepare_datasets(twitter_data):
     sentences = twitter_data['tweets'].values
     sentiment_scores = twitter_data['sentiment'].values
 
+    t0 = time.time()
+    print('Encoding sentences...')
     for split in split_inds:
         inds = split_inds[split]
-        data_splits[split]['sentences'] = sentences[inds]
+        data_splits[split]['embeddings'] = get_sent_embeddings(tokenizer1, encoder1, tokenizer2, encoder2, device, sentences[inds].tolist())
         data_splits[split]['sent_labels'] = sentiment_scores[inds]
+    print(f'Done in {round(time.time() - t0, 2)} seconds')
         
     return TweetDataset(data_splits['train']), TweetDataset(data_splits['val']), TweetDataset(data_splits['test'])
 
@@ -50,16 +56,18 @@ def init_model(hparams, n_classes=3):
     return model, criterion, optimizer 
 
 
-def train(model, optimizer, criterion, train_dataset, val_dataset, batch_collater, hparams):
+def train(model, optimizer, criterion, train_dataset, val_dataset, hparams):
     epoch_train_losses = []
     epoch_val_losses = []
     epoch_val_accs = []
 
-    train_loader = DataLoader(train_dataset, batch_size=hparams['batch_size'], collate_fn=batch_collater, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=hparams['batch_size'], shuffle=True)
+    model = model.to(device)
+
     for epoch in range(hparams['num_epochs']):
         train_losses_sub = []
         
-        for embeddings_train, labels_train, sentences_train in tqdm(train_loader, desc=f'epoch {epoch}', leave=False):
+        for embeddings_train, labels_train in train_loader:
             optimizer.zero_grad()
             train_outputs = model.forward(embeddings_train.to(device))
             train_loss = criterion(train_outputs, labels_train.to(device))
@@ -72,7 +80,7 @@ def train(model, optimizer, criterion, train_dataset, val_dataset, batch_collate
         epoch_loss = np.mean(train_losses_sub)
         epoch_train_losses.append(epoch_loss)
 
-        epoch_val_acc, epoch_val_loss = eval(model, criterion, val_dataset, hparams, batch_collater)
+        epoch_val_acc, epoch_val_loss = eval(model, criterion, val_dataset, hparams)
         epoch_val_accs.append(epoch_val_acc)
         epoch_val_losses.append(epoch_val_loss)
     
@@ -84,14 +92,14 @@ def accuracy(preds, trues):
     return matches.sum().item() / len(trues)
 
 
-def eval(model, criterion, val_dataset, hparams, batch_collater):
-    val_loader = DataLoader(val_dataset, batch_size=hparams['batch_size'], collate_fn=batch_collater, shuffle=False)
+def eval(model, criterion, val_dataset, hparams):
+    val_loader = DataLoader(val_dataset, batch_size=hparams['batch_size'], shuffle=False)
     val_accs = []
     val_losses = []
     
     model.eval()
     with torch.no_grad():
-        for embeddings_val, labels_val, sentences_val in val_loader:
+        for embeddings_val, labels_val in val_loader:
             val_outputs = model.forward(embeddings_val.to(device))
             val_loss = criterion(val_outputs, labels_val.to(device))
 
@@ -102,13 +110,10 @@ def eval(model, criterion, val_dataset, hparams, batch_collater):
     return np.mean(val_accs), np.mean(val_losses)
 
 
-def train_classifier(tokenizer1, encoder1, data, hparams, tokenizer2=None, encoder2=None):
-
-    train_dataset, val_dataset, test_dataset = prepare_datasets(data)
-    batch_collater = partial(tweet_batch_collate, tokenizer1=tokenizer1, encoder1=encoder1, tokenizer2=tokenizer2, encoder2=encoder2)
+def train_classifier(train_dataset, val_dataset, hparams):
 
     model, criterion, optimizer = init_model(hparams)
-    epoch_train_losses, epoch_val_losses, epoch_val_accs = train(model, optimizer, criterion, train_dataset, val_dataset, batch_collater, hparams)
+    epoch_train_losses, epoch_val_losses, epoch_val_accs = train(model, optimizer, criterion, train_dataset, val_dataset, hparams)
 
     epoch_results = pd.DataFrame()
     epoch_results['epoch'] = np.arange(len(epoch_train_losses))
@@ -125,14 +130,14 @@ def train_classifier(tokenizer1, encoder1, data, hparams, tokenizer2=None, encod
         epoch_results.to_csv('experiment_results.csv', mode='a', index=False, header=False)
 
 
-def get_hparams(tune):
+def get_hparams(args, input_dim):
     
-    if tune == True:
+    if args.tune == True:
         hidden_dims = [64, 128, 256, 512]
-        learning_rate = [5e-4, 5e-3, 5e-2, 5e-1]
-        num_epochs = [10]
-        batch_size = [64]
-        n_layers = [0, 1, 2]
+        learning_rate = [5e-6, 5e-5, 5e-4, 5e-3, 5e-2]
+        num_epochs = [NEPOCHS]
+        batch_size = [BATCHSIZE]
+        n_layers = [0, 1, 2, 3, 4]
 
         hparam_sets = []
         for hparams in list(itertools.product(*[hidden_dims, learning_rate, num_epochs, batch_size, n_layers])):
@@ -140,9 +145,11 @@ def get_hparams(tune):
                                 'learning_rate': hparams[1], 
                                 'num_epochs': hparams[2],
                                 'batch_size': hparams[3],
-                                'n_layers': hparams[4]})
+                                'n_layers': hparams[4],
+                                'model': args.model,
+                                'input_dim': input_dim})
     else:
-        hparam_sets = [{'hidden_dim': 256, 'learning_rate': 5e-3, 'num_epochs': 20, 'batch_size': 32, 'n_layers': 0}]
+        hparam_sets = [{'hidden_dim': 256, 'learning_rate': 5e-3, 'num_epochs': 20, 'batch_size': 32, 'n_layers': 0, 'model': args.model, 'input_dim': input_dim}]
 
     return hparam_sets
 
@@ -164,27 +171,31 @@ if __name__ == '__main__':
     args = parse_args()
     models = {'simcse': 'princeton-nlp/sup-simcse-bert-base-uncased',
               'bert': 'bert-base-uncased'}
-
     twitter_data = pd.read_csv(args.path_to_data)
+    
 
-    hparam_sets = get_hparams(args.tune)
-    for hparams in hparam_sets:
-        if args.model == 'simcse' or args.model == 'bert':
-            tokenizer = AutoTokenizer.from_pretrained(models[args.model])
-            encoder = AutoModel.from_pretrained(models[args.model])
-            hparams['input_dim'] = encoder.embeddings.token_type_embeddings.embedding_dim
-            train_classifier(tokenizer, encoder, twitter_data, hparams)
-
-        elif args.model == 'both':
-            tokenizer1 = AutoTokenizer.from_pretrained(models['simcse'])
-            encoder1 = AutoModel.from_pretrained(models['simcse'])
-            tokenizer2 = AutoTokenizer.from_pretrained(models['bert'])
-            encoder2 = AutoModel.from_pretrained(models['bert'])
-            hparams['input_dim'] = encoder1.embeddings.token_type_embeddings.embedding_dim + \
-                                encoder2.embeddings.token_type_embeddings.embedding_dim
-            train_classifier(tokenizer1, encoder1, twitter_data, hparams, tokenizer2, encoder2)
+    if args.model == 'simcse' or args.model == 'bert':
+        tokenizer = AutoTokenizer.from_pretrained(models[args.model])
+        encoder = AutoModel.from_pretrained(models[args.model]).to(device)
+        train_dataset, val_dataset, test_dataset = prepare_datasets(twitter_data, tokenizer, encoder)
+        input_dim = encoder.embeddings.token_type_embeddings.embedding_dim
+        hparam_sets = get_hparams(args, input_dim)
+    
+    elif args.model == 'both':
+        tokenizer1 = AutoTokenizer.from_pretrained(models['simcse'])
+        encoder1 = AutoModel.from_pretrained(models['simcse']).to(device)
+        tokenizer2 = AutoTokenizer.from_pretrained(models['bert'])
+        encoder2 = AutoModel.from_pretrained(models['bert']).to(device)
+        train_dataset, val_dataset, test_dataset = prepare_datasets(twitter_data, tokenizer1, encoder1, tokenizer2, encoder2)
+        input_dim = encoder1.embeddings.token_type_embeddings.embedding_dim + encoder2.embeddings.token_type_embeddings.embedding_dim
+        hparam_sets = get_hparams(args, input_dim)
     
     else:
         raise ValueError(f'Invalid choice of model: {args.model}')
+
+    for hparams in tqdm(hparam_sets, desc=f'running tuning experiments'):
+        train_classifier(train_dataset, val_dataset, hparams)
+    
+    
     
             
--- project_extension/tweet_dataset.py
+++ project_extension/tweet_dataset.py
@@ -8,7 +8,7 @@ class TweetDataset(Dataset):
     """
 
     def __init__(self, data):
-        self.sentences = data['sentences']
+        self.embeddings = data['embeddings']
         self.sent_labels = data['sent_labels']
         
     def __len__(self):
@@ -19,27 +19,19 @@ class TweetDataset(Dataset):
         Triggered when you call dataset[i]
         """
 
-        return (self.sentences[idx], self.sent_labels[idx])
+        return (self.embeddings[idx], self.sent_labels[idx])
 
 
-def get_sent_embeddings(tokenizer, encoder, sentences):
-    inputs = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
-
+def get_sent_embeddings(tokenizer1, encoder1, tokenizer2, encoder2, device, sentences):
+    inputs1 = tokenizer1(sentences, padding=True, truncation=True, return_tensors='pt').to(device)
     with torch.no_grad():
-        embeddings = encoder(**inputs, output_hidden_states=True, return_dict=True).pooler_output
-    
-    return embeddings
-
-
-def tweet_batch_collate(batch, tokenizer1, encoder1, tokenizer2, encoder2):
-    sentences = [b[0] for b in batch]
-    labels = torch.LongTensor([b[1] for b in batch])
-
-    embeddings = get_sent_embeddings(tokenizer1, encoder1, sentences)
+        embeddings = encoder1(**inputs1, output_hidden_states=True, return_dict=True).pooler_output
     
     if tokenizer2 and encoder2:
-        embeddings2 = get_sent_embeddings(tokenizer2, encoder2, sentences)
-        embeddings = torch.cat((embeddings, embeddings2), dim=1)
+        inputs2 = tokenizer2(sentences, padding=True, truncation=True, return_tensors='pt').to(device)
+        embeddings2 = encoder2(**inputs2, output_hidden_states=True, return_dict=True).pooler_output
 
-    return (embeddings, labels, sentences)
+        embeddings = torch.cat((embeddings, embeddings2), dim=1)
+    
+    return embeddings
 
